747942b58ce51218018a6fb3462264b3407a58d3
shoumen-shomu <shoumenshomu@gmail.com>
2026-06-15 17:17:44 +0600

    Added Filter Method

--- script.js
+++ script.js
@@ -347,4 +347,63 @@ console.log(month); //(4) ['Jan', 'Feb', 'Mar', 'Dec']
   
   
   
+// The Customer Array
+let customers = [
+  {
+     'id': 001,
+     'f_name': 'Abby',
+     'l_name': 'Thomas',
+     'gender': 'M',
+     'married': true,
+     'age': 32,
+     'expense': 500,
+     'purchased': ['Shampoo', 'Toys', 'Book']
+  },
+  {
+     'id': 002,
+     'f_name': 'Jerry',
+     'l_name': 'Tom',
+     'gender': 'M',
+     'married': true,
+     'age': 64,
+     'expense': 100,
+     'purchased': ['Stick', 'Blade']
+  },
+  {
+     'id': 003,
+     'f_name': 'Dianna',
+     'l_name': 'Cherry',
+     'gender': 'F',
+     'married': true,
+     'age': 22,
+     'expense': 1500,
+     'purchased': ['Lipstik', 'Nail Polish', 'Bag', 'Book']
+  },
+  {
+     'id': 004,
+     'f_name': 'Dev',
+     'l_name': 'Currian',
+     'gender': 'M',
+     'married': true,
+     'age': 82,
+     'expense': 90,
+     'purchased': ['Book']
+  },
+  {
+     'id': 005,
+     'f_name': 'Maria',
+     'l_name': 'Gomes',
+     'gender': 'F',
+     'married': false,
+     'age': 7,
+     'expense': 300,
+     'purchased': ['Toys']
+  }
+];
 
+//Filter Method
+
+const seniorCustomer = customers.filter((customer) =>{
+  return (customer.age>=60)
+})
+console.log("Senior Citizen" ,seniorCustomer );
